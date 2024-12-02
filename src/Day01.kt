84417a2679b9a4536d3abada2f59ff6d2ad797aa
Niels Marsman <niels.marsman@luminis.eu>
2024-12-02 10:04:58 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,54 @@
+import kotlin.collections.unzip
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::splitInPair)
+            .unzip()
+            .sorted()
+            .distances()
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::splitInPair)
+            .unzip()
+            .sorted()
+            .similarities()
+            .sum()
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun String.splitInPair() : Pair<Int, Int> = split("\\s+".toRegex())
+    .map(String::toInt)
+    .let { it.first() to it.last() }
+
+private fun Pair<List<Int>, List<Int>>.sorted(): Pair<List<Int>, List<Int>> {
+    return copy(
+        first.sorted(),
+        second.sorted()
+    )
+}
+
+private fun Pair<List<Int>, List<Int>>.distances(): List<Int> {
+    return first.mapIndexed { index, item ->
+        abs(item - second[index])
+    }
+}
+
+private fun Pair<List<Int>, List<Int>>.similarities(): List<Int> {
+    return first.map { item ->
+        item * second.filter { it == item }.size
+    }
 }
